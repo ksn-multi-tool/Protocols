0b86a2081e2e445553d35f9dfb18eb1d9fc717c1
KSN <redawewrq12@gmail.com>
2025-08-06 00:20:40 +0300

    Create spd_processor.cpp

---
+++ processors/spd_processor.cpp
@@ -0,0 +1,41 @@
+#include "spd_processor.h"
+
+SPDProcessor::SPDProcessor(KeyManager& key_manager) : key_manager_(key_manager) {}
+
+bool SPDProcessor::SetBlockSize(uint16_t size) {
+    return SPDProtocol::SetBlockSize(size);
+}
+
+bool SPDProcessor::ReadPartition(const std::string& part_spec, const std::string& output) {
+    if (part_spec == "all" || part_spec == "all_lite") {
+        bool lite = (part_spec == "all_lite");
+        for (const auto& [name, info] : partition_table_) {
+            if (lite && (name == "blackbox" || name == "cache" || name == "userdata")) 
+                continue;
+            
+            std::string out_file = output + "/" + name + ".bin";
+            auto data = SPDProtocol::ReadPartition(name, 0, info.size);
+            
+            std::ofstream file(out_file, std::ios::binary);
+            file.write(reinterpret_cast<char*>(data.data()), data.size());
+        }
+        return true;
+    }
+    
+    auto data = SPDProtocol::ReadPartition(part_spec, 0, partition_table_[part_spec].size);
+    std::ofstream file(output, std::ios::binary);
+    file.write(reinterpret_cast<char*>(data.data()), data.size());
+    return true;
+}
+
+bool SPDProcessor::WritePartition(const std::string& part_spec, const std::string& input) {
+    std::ifstream in_file(input, std::ios::binary | std::ios::ate);
+    if (!in_file) return false;
+    
+    size_t size = in_file.tellg();
+    in_file.seekg(0, std::ios::beg);
+    std::vector<uint8_t> data(size);
+    in_file.read(reinterpret_cast<char*>(data.data()), size);
+    
+    return SPDProtocol::WritePartition(part_spec, data);
+}
